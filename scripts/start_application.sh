297d2d1fb2fcdd50ba9b61ea1fa82a963439161d
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-24 04:46:01 +0100

    added command

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,17 +1,17 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Start Tomcat
-# echo "Starting Tomcat service..."
-# sudo systemctl start tomcat
+# Start Tomcat
+echo "Starting Tomcat service..."
+sudo systemctl start tomcat
 
-# # Wait for a few seconds to give Tomcat some time to start
-# sleep 5
+# Wait for a few seconds to give Tomcat some time to start
+sleep 5
 
-# # Check if Tomcat started successfully
-# if sudo systemctl is-active --quiet tomcat; then
-#   echo "Tomcat started successfully."
-# else
-#   echo "Failed to start Tomcat. Checking status for more information..."
-#   sudo systemctl status tomcat
-#   exit 1
-# fi
+# Check if Tomcat started successfully
+if sudo systemctl is-active --quiet tomcat; then
+  echo "Tomcat started successfully."
+else
+  echo "Failed to start Tomcat. Checking status for more information..."
+  sudo systemctl status tomcat
+  exit 1
+fi
